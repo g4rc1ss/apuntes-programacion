6660efdc61462f0c4260476f9d1d1c0242e90aa8
Asier <asiergarciab@outlook.com>
2025-06-27 23:01:53 +0200

    Update MainWindow.axaml.cs

--- Lenguajes/Dotnet/ApuntesNet/BackendLibraries/Performance/AsyncOps/MainWindow.axaml.cs
+++ Lenguajes/Dotnet/ApuntesNet/BackendLibraries/Performance/AsyncOps/MainWindow.axaml.cs
@@ -54,6 +54,6 @@ public partial class MainWindow : Window
     private async void ExecuteWithPlatformInvokeAsync(object? sender, RoutedEventArgs e)
     {
         await new ClaseInteractuaRust().EjecutarDllAsync();
-        File.WriteAllTextAsync("./archivo.txt", "Hola mundo");
+        await Task.Delay(100);
     }
 }
